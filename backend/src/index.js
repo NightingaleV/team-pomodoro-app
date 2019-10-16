be1c4c11c1890e3ade2964e278fc4d87cca61fe6
Vitezslav Slavik <vitezslavik@gmail.com>
2019-10-16 20:00:37 +0200

    Add backend dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,19 @@
 import express from "express";
+// Import Env Variables
+const { parsed, error } = require("dotenv").config({
+  path: "../config/.env",
+  debug: true
+});
+console.error(error);
+console.log(parsed);
 
+// Init a Express.js
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/api/data/", (req, res) => {
   console.log("what");
+  console.log(process.env.DB_USER);
   res.send('{"express_status": "working"}');
 });
 
