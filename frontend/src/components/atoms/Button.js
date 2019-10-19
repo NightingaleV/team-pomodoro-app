c3358d6fb1a5176be0776109c7fa5fd8fbf3a6fe
Vitezslav Slavik <vitezslavik@gmail.com>
2019-10-19 13:34:57 +0200

    Create Button Component

---
+++ frontend/src/components/atoms/Button.js
@@ -0,0 +1,56 @@
+import React from "react";
+import classNames from "classnames";
+
+const DEFAULT_BTN_CLASSES = "waves-effect waves-light";
+
+const BUTTON_SHAPES = {
+  defaultBtn: DEFAULT_BTN_CLASSES,
+  bigCircular: "btn-floating btn-large waves-effect waves-light",
+  bigBtn: "waves-effect waves-light btn-large"
+};
+
+const ACTION_BUTTONS = {
+  play: {
+    color: "",
+    icon: "play_arrow"
+  },
+  pause: {
+    color: "amber",
+    icon: "pause"
+  },
+  stop: {
+    color: "red",
+    icon: "stop"
+  },
+  restart: {
+    color: "orange",
+    icon: "replay"
+  }
+};
+
+export function Button({
+  children,
+  shape,
+  color,
+  icon,
+  actionButton,
+  className,
+  iconPosition,
+  ...rest
+}) {
+  const shapeClasses = BUTTON_SHAPES[shape] || DEFAULT_BTN_CLASSES;
+
+  color = color || ACTION_BUTTONS[actionButton].color;
+  icon = icon || ACTION_BUTTONS[actionButton].icon;
+  iconPosition = iconPosition || "left";
+  return (
+    <button
+      className={classNames("btn", color, shapeClasses, className)}
+      type="button"
+      {...rest}
+    >
+      <i className={classNames("material-icons", iconPosition)}>{icon}</i>
+      {children}
+    </button>
+  );
+}
