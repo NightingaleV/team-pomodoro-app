b12baa40e0ee99182b39119b26b3de3ec9c371f9
Vitezslav Slavik <vitezslavik@gmail.com>
2019-10-27 15:17:08 +0100

    Connect backend to Mongo Dev DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,54 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
 // Import Env Variables
-const { parsed, error } = require("dotenv").config({
-  path: "../config/dev.env",
-  debug: true
+const { parsed, error } = require('dotenv').config({
+  path: '../config/dev.env',
+  debug: true,
 });
 console.error(error);
 // Env Variables parsed to disposal
 console.log(parsed);
 
+// Routers
+import { userRouter } from './routes/user';
+
 // Init a Express.js
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/api/data/", (req, res) => {
-  res.send('{"express_status": "working"}');
-});
+// SET DB Connection
+const DB = process.env.DB_URI;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    mongoose.set('useFindAndModify', false);
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.log('error statement');
+    console.log(err.message);
+    process.exit(1);
+  }
+};
 
-app.get("*", (req, res) => {
-  res.send("Express is working.");
-});
+// Establish DB connection
+connectDB().then(value => {
+  app.get('/', (req, res) => res.send('API Running'));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  app.get('/api/data/', (req, res) => {
+    res.send('{"express_status": "working"}');
+  });
+
+  // Init Middleware
+  // Default body-parser
+  app.use(express.json({ extended: false }));
+
+  // Define Routes
+  app.use('/api/user', userRouter);
+
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+});
