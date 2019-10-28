d88e4af1104ff563ed30a0b89a99662bea1ab5d3
Vitezslav Slavik <vitezslavik@gmail.com>
2019-10-28 19:13:08 +0100

    Implement user login backend route

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,13 +1,28 @@
 // External imports
 import express from 'express';
+// import verifyToken from '../../middleware/auth';
 import { check, validationResult } from 'express-validator';
-// import gravatar from 'gravatar';
+import bcrypt from 'bcryptjs';
 
 // Internal imports
 import { User } from '../models/User';
+import auth from '../../../middleware/auth';
 
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+
+// @route   GET api/user/
+// @desc    get user by token
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    await res.json({ user: user });
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
 
 // @route   POST api/user/register
 // @desc    Registration of user account
@@ -58,11 +73,85 @@ router.post(
         password,
       });
       // save user to database
-      user.save(function(err) {
-        if (err) throw err;
-      });
+      user
+        .save(function(err) {
+          if (err) throw err;
+        })
+        .then(user => {
+          // Return json web token for authentication
+          const payload = {
+            user: { id: user.id },
+          };
+          // Send jwt auth token
+          jwt.sign(
+            payload,
+            process.env.JWTPrivateKey,
+            {
+              expiresIn: 360000,
+            },
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                token,
+              });
+            },
+          );
+        });
 
-      // Return json web token for authentication
+      //return res.send('User Registered')
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send('Server Error, Try it later');
+    }
+
+    console.log(req.body);
+    //res.send('User1 Test Route');
+  },
+);
+
+// @route    POST api/user/login
+// @desc     Authenticate user & get token
+// @access   Public
+router.post(
+  '/login',
+  [
+    check('email', 'Email is invalid').isEmail(),
+    check('password', 'Please enter a password').exists(),
+  ],
+  async (req, res) => {
+    // Return validation
+    const errors = validationResult(req);
+    process.stdout.write(errors);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      // User with that email not found
+      if (!user) {
+        return res.status(400).json({
+          errors: [
+            {
+              msg: 'Meh, we were unable to find you using these credentials.',
+            },
+          ],
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({
+          errors: [
+            {
+              msg: 'Meh, we were unable to find you using these credentials.',
+            },
+          ],
+        });
+      }
+
+      // Return jsonwebtoken for authentication
       const payload = {
         user: { id: user.id },
       };
@@ -71,10 +160,11 @@ router.post(
         payload,
         process.env.JWTPrivateKey,
         {
-          expiresIn: 360000,
+          expiresIn: 3600,
         },
         (err, token) => {
           if (err) throw err;
+          // return res.cookie('token', token, { httpOnly: true }).sendStatus(200);
           res.json({
             token,
           });
@@ -87,7 +177,7 @@ router.post(
       return res.status(500).send('Server Error, Try it later');
     }
 
-    console.log(req.body);
+    // console.log(req.body);
     //res.send('User1 Test Route');
   },
 );
