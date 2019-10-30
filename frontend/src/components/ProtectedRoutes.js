fd0fdc3033ca1d9826d0fb1df740c510fd3b7ba4
Vitezslav Slavik <vitezslavik@gmail.com>
2019-10-30 14:53:00 +0100

    Update ProtectedRoutes to work with Auth provider

--- frontend/src/components/ProtectedRoutes.js
+++ frontend/src/components/ProtectedRoutes.js
@@ -1,27 +1,11 @@
 import React, { Component, useState, useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import axios from 'axios';
+import { useAuth } from '../utils/useAuth';
 
 export function ProtectedRoute({ component: Component, ...rest }) {
   // const [isAuthenticated, setIsAuthenticated] = useState(false);
-  let isAuthenticated = false;
-  const authenticate = async () => {
-    const token = localStorage.getItem('token');
-    const config = {
-      headers: {
-        'x-auth-token': token,
-      },
-      withCredentials: true,
-    };
-    const res = await axios.get('/api/user/', config);
-    if (res.data.user) {
-      isAuthenticated = true;
-    }
-  };
-  useEffect(() => {
-    authenticate();
-    console.log(isAuthenticated);
-  }, []);
+  const { user, signout } = useAuth();
 
   function protectComponent(props) {}
 
@@ -29,11 +13,11 @@ export function ProtectedRoute({ component: Component, ...rest }) {
     <Route
       {...rest}
       render={props => {
-        if (isAuthenticated) {
-          console.log('Valid');
+        if (user) {
+          console.log('Valid User');
           return <Component {...props} />;
         } else {
-          console.log('InValid');
+          console.log('InValid User');
           return (
             <Redirect
               to={{
@@ -46,28 +30,4 @@ export function ProtectedRoute({ component: Component, ...rest }) {
       }}
     />
   );
-
-  // return (
-  //   <Route
-  //     {...rest}
-  //     render={props => {
-  //       if (isAuthenticated) {
-  //         console.log('Valid');
-  //         return <Component {...props} />;
-  //       } else {
-  //         console.log('InValid');
-  //         return (
-  //           <Redirect
-  //             to={{
-  //               pathname: '/login',
-  //               state: {
-  //                 from: props.location,
-  //               },
-  //             }}
-  //           />
-  //         );
-  //       }
-  //     }}
-  //   />
-  // );
 }
