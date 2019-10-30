4d680edaf517036ddc1a5fac42dec71c54621c2b
David Smahel <smad00@vse.cz>
2019-10-30 21:53:07 +0100

    Refactor GroupDetail

--- frontend/src/views/GroupDetail.js
+++ frontend/src/views/GroupDetail.js
@@ -1,5 +1,5 @@
 // External imports
-import React, { Fragment, useState, Component } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import classNames from 'classnames';
 import axios from 'axios';
@@ -7,75 +7,57 @@ import axios from 'axios';
 // Internal imports
 import { TopNavigation } from '../components/organisms';
 
-export class GroupDetail extends Component{    
+export function GroupDetail(){
+    const [group, setGroup] = useState({name: "", members: []});
 
-    constructor(props){
-        super(props);
-        this.state={
-            group: {name: "", members: []}
-        }
-    }
-
-    async getData(){
-      try {
-        const config = {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          params:{
-              'groupName': 'Testovací skupina'
-          }
-        };
-
-        const res = await axios.get('api/group/detail', config);
-        console.log('Valid Statement');
-        console.log(res.data);
-        this.setState({
-            group: res.data.filterGroup[0]
-        })
-
-        console.log(this.state.group);
-
-      } catch (err) {
-        console.log('Error Statement');
-        // console.error(err.response.data);
-        // setError({ ...errors, backend: err.response.data.errors });
-        // err.response.data.errors.map(error => {
-        //   console.log(error);
-        // });
-      }
-    }
-
-    componentDidMount(){
-        this.getData();
-    }
-
-    render(){  
-        const members = this.state.group.members;
+    useEffect(() => {
+        async function fetchData(){
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    params:{
+                        'groupName': 'Testovací skupina'
+                    }
+                };
+                        
+                const res = await axios.get('api/group/detail', config);                
+                console.log("Result:", res.data);
+                setGroup(res.data.filterGroup[0]);     
         
-        return(            
-            <Fragment>
-            <TopNavigation></TopNavigation>
-            <div className="blue lighten-5">
-                <div className="container">
-                    <div className="row">
-                      <div className="col s12 center-align">
-                        <h3>Group:
-                            {this.state.group.name}
-                        </h3>                        
+            } catch (err) {
+                console.log('Error Statement');
+            }
+        }
 
-                        <div>
-                            {this.state.group.members.map((member, index) => (
-                                <div>
-                                   <p>Name: {member.email} Status: {member.timer.status}</p>
-                                </div>
-                            ))}
-                        </div>
-                      </div>
+        fetchData();        
+    }, []);
+
+    console.log("Group:", group);
+
+    return(            
+        <Fragment>
+        <TopNavigation></TopNavigation>        
+        <div className="blue lighten-5">
+            <div className="container">
+                <div className="row">
+                  <div className="col s12 center-align">
+                    <h3>Group:
+                        {group.name}
+                    </h3>                        
+
+                    <div>
+                        {group.members.map((member, index) => (
+                            <div key={index}>
+                               <p>Name: {member.email} Status: {member.timer.status}</p>
+                            </div>
+                        ))}
                     </div>
+                  </div>
                 </div>
             </div>
-            </Fragment>
-        );
-    }
+        </div>
+        </Fragment>
+    );    
 }
