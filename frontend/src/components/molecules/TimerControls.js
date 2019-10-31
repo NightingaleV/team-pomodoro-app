39d1f6a760f08ff70dbb73dcc3e9823661add39a
Vitezslav Slavik <vitezslavik@gmail.com>
2019-10-31 17:33:58 +0100

    Implement dropdown menu inside timer controls

--- frontend/src/components/molecules/TimerControls.js
+++ frontend/src/components/molecules/TimerControls.js
@@ -1,30 +1,90 @@
 // External imports
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 // Internal imports
 import { Button } from '../atoms';
+import { useAuth } from '../../utils/useAuth';
+import { TopNavigation } from '../organisms';
+import { SignUpSuccess } from '../../templates';
+import M from 'materialize-css';
+
+export function DropdownTimerMenu(props) {
+  const { user } = useAuth();
+  function initDropdownMenu() {
+    const dropdownMenuElement = document.querySelectorAll('.dropdown-trigger');
+    const options = { constrainWidth: false };
+    M.Dropdown.init(dropdownMenuElement, options);
+    // dropdown.open();
+  }
+
+  useEffect(() => {
+    initDropdownMenu();
+  }, []);
+
+  return (
+    <Fragment>
+      <Button
+        shape={'bigBtn'}
+        actionButton={'dropdown'}
+        className={'dropdown-trigger btn teal'}
+        data-target="dropdown1"
+      ></Button>
+      <ul
+        id="dropdown1"
+        className="dropdown-content"
+        style={{ minWidth: '165px' }}
+      >
+        <li>
+          <a onClick={props.controlMethods.setWork}>Work</a>
+        </li>
+        <li>
+          <a onClick={props.controlMethods.setShortBreak}>Take a break</a>
+        </li>
+        <li>
+          <a onClick={props.controlMethods.setLongBreak}>Take a long break</a>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
 
 export function TimerControls(props) {
   let CTA = 'Call to Action';
-  const { isRunning, handleClick, type, children } = props;
+  const { isRunning, type, children } = props;
   if (type === 1) CTA = '';
   if (type === 2) CTA = '';
   if (type === 3) CTA = '';
-  if (isRunning) {
+
+  const {
+    startTimer,
+    pauseTimer,
+    nextTimer,
+    restartTimer,
+  } = props.controlHandlers;
+
+  if (!isRunning) {
     return (
       <Fragment>
-        <Button shape={'bigBtn'} actionButton={'stop'} onClick={handleClick[0]}>
-          {children}
+        <Button shape={'bigBtn'} actionButton={'play'} onClick={startTimer}>
+          {CTA} {children}
         </Button>
-          <Button shape={'bigBtn'} actionButton={'pause'} onClick={handleClick[1]}>
-          {children}
-        </Button>
+        <DropdownTimerMenu controlMethods={props.dropdownControlHandlers} />
       </Fragment>
     );
   } else {
     return (
       <Fragment>
-        <Button shape={'bigBtn'} actionButton={'play'} onClick={handleClick}>
-          {CTA} {children}
+        <Button shape={'bigBtn'} actionButton={'stop'} onClick={nextTimer}>
+          {children}
+        </Button>
+        <Button shape={'bigBtn'} actionButton={'pause'} onClick={pauseTimer}>
+          {children}
+        </Button>
+        <Button
+          shape={'bigBtn'}
+          actionButton={'restart'}
+          onClick={restartTimer}
+        >
+          {children}
         </Button>
       </Fragment>
     );
