0e145f5cc2af5c936d115781bee7f901f916d96c
Vitezslav Slavik <vitezslavik@gmail.com>
2019-11-08 20:41:16 +0100

    Create Route for updating Timer

--- backend/src/controllers/timer.js
+++ backend/src/controllers/timer.js
@@ -2,6 +2,7 @@
 
 //Internal imports
 import { Timer } from '../models/Timer';
+import { User } from '../models/User';
 
 // LOGIC
 //------------------------------------------------------------------------------
@@ -16,17 +17,24 @@ export async function selectTimer(req, res) {
 }
 
 export async function createTimer(req, res) {
-  const { type, name, totTime, remTime, userID, isRunning } = req.body;
+  // const { type, name, totTime, remTime, userID, isRunning } = req.body;
+  const {
+    isRunning,
+    remTime,
+    settings,
+    progressBar,
+    userID,
+    indexInCycle,
+  } = req.body;
   try {
     let timer;
 
     timer = new Timer({
-      type,
-      name,
-      totTime,
-      remTime,
-      userID,
       isRunning,
+      remTime,
+      settings,
+      indexInCycle,
+      userID,
     });
 
     //save timer to database
@@ -42,6 +50,35 @@ export async function createTimer(req, res) {
         }
       },
     );
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send('Server Error, Try it later');
+  }
+
+  console.log(req.body);
+}
+
+export async function updateTimer(req, res) {
+  const {
+    timerID,
+    isRunning,
+    remTime,
+    settings,
+    userID,
+    indexInCycle,
+  } = req.body;
+  try {
+    const updatedTimer = {
+      isRunning,
+      remTime,
+      settings,
+      userID,
+      indexInCycle,
+    };
+    let timer = await Timer.findOneAndUpdate({ _id: timerID }, updatedTimer, {
+      new: true,
+    });
+    await res.json({ timer });
   } catch (err) {
     console.log(err);
     return res.status(500).send('Server Error, Try it later');
--- backend/src/routes/timer.js
+++ backend/src/routes/timer.js
@@ -2,9 +2,12 @@
 import express from 'express';
 
 //Internal imports
-import { selectTimer, createTimer } from '../controllers/timer';
+import { selectTimer, createTimer, updateTimer } from '../controllers/timer';
+import auth from '../../../middleware/auth';
 
 const router = express.Router();
+
+//TODO make routes protected, also give it header to postman
 
 // @route   GET api/timer/
 // @desc    get timer by userID
@@ -15,5 +18,10 @@ router.get('/', selectTimer);
 // @desc    Save timer status
 // @access  Private
 router.post('/save', createTimer);
+
+// @route   POST api/timer/save
+// @desc    Save timer status
+// @access  Private
+router.post('/update', updateTimer);
 
 export { router as timerRouter };
