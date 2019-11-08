87adcb5492303d6b78806fe42a186489f35a07eb
Vitezslav Slavik <vitezslavik@gmail.com>
2019-11-08 20:41:01 +0100

    Update Timer Schema

--- backend/src/models/Timer.js
+++ backend/src/models/Timer.js
@@ -1,21 +1,29 @@
 import mongoose from 'mongoose';
 
 const TimerSchema = new mongoose.Schema({
-  type: {
-    type: Number,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  totTime: {
-    type: Number,
-    required: true,
+  isRunning: {
+    type: Boolean,
   },
   remTime: {
     type: Number,
     required: true,
+  },
+  settings: {
+    type: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    totTime: {
+      type: Number,
+      required: true,
+    },
+  },
+  indexInCycle: {
+    type: Number,
   },
   userID: {
     type: mongoose.Schema.Types.ObjectId,
@@ -23,15 +31,11 @@ const TimerSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    required: true,
     default: Date.now,
   },
   updatedAt: {
     type: Date,
-  },
-  isRunning: {
-    type: Boolean,
-    required: true,
+    default: Date.now,
   },
 });
 
@@ -39,6 +43,16 @@ TimerSchema.pre('save', function(next) {
   let timer = this;
   timer.updatedAt = Date.now();
   next();
+});
+
+TimerSchema.pre('findOneAndUpdate', function(next) {
+  const updatedTimer = this.getUpdate();
+  try {
+    updatedTimer.updatedAt = Date.now();
+    next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 export const Timer = mongoose.model('Timer', TimerSchema);
