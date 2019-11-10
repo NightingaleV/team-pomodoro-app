17c14040344afd254ca51a653fd4637114be96d8
Vitezslav Slavik <vitezslavik@gmail.com>
2019-11-10 19:47:21 +0100

    Refactor middleware

--- backend/src/routes/timer.js
+++ backend/src/routes/timer.js
@@ -8,11 +8,9 @@ import {
   updateTimer,
   selectLastTimer,
 } from '../controllers/timer';
-import auth from '../../../middleware/auth';
+import auth from '../middleware/auth';
 
 const router = express.Router();
-
-//TODO make routes protected, also give it header to postman
 
 // @route   GET api/timer/
 // @desc    get timer by userID
