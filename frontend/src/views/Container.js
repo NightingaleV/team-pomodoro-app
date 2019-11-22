f8068978e04828c53f9744848a0f1eac8f41e560
Vitezslav Slavik <vitezslavik@gmail.com>
2019-11-22 20:28:13 +0100

    Hide sidebar when anonymous

--- frontend/src/views/Container.js
+++ frontend/src/views/Container.js
@@ -7,15 +7,16 @@ import {
   FooterNavigation,
   CreateGroupModal,
 } from '../components/organisms';
+import { useAuth } from '../utils/useAuth';
 
 export function Container(props) {
-  return (
+  const { user, token } = useAuth();
+  const layoutWhenLogedIn = (
     <>
-      <TopNavigation></TopNavigation>
-
+      <TopNavigation />
       <div className={'row content'}>
         <div className={'col l2 hide-on-small-only'}>
-          <SideNavigation></SideNavigation>
+          {user && <SideNavigation />}
         </div>
         <div className={'col l10 m12 s12   center-align'}>
           <div className="container">{props.children}</div>
@@ -27,4 +28,21 @@ export function Container(props) {
       <FooterNavigation></FooterNavigation>
     </>
   );
+
+  const layoutWhenAnonymous = (
+    <>
+      <TopNavigation />
+      <div className={'row content'}>
+        <div className={'col l12 m12 s12   center-align'}>
+          <div className="container">{props.children}</div>
+        </div>
+      </div>
+      <div className="modals-container">
+        <CreateGroupModal />
+      </div>
+      <FooterNavigation></FooterNavigation>
+    </>
+  );
+
+  return user ? layoutWhenLogedIn : layoutWhenAnonymous;
 }
