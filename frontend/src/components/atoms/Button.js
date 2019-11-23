fff4dfce42dc38dcc257059e67c89c43226b81f7
Vitezslav Slavik <vitezslavik@gmail.com>
2019-11-23 18:11:22 +0100

    Create & Update Invitation modal

--- frontend/src/components/atoms/Button.js
+++ frontend/src/components/atoms/Button.js
@@ -3,21 +3,22 @@ import React from 'react';
 import classNames from 'classnames';
 
 //TODO REMOVE COLOR FROM DEFAULT CLASSES
-const DEFAULT_BTN_CLASSES = 'waves-effect waves-light amber';
+const DEFAULT_BTN_CLASSES = 'waves-effect waves-light';
+const DEFAULT_COLOR = 'amber';
 
 const BUTTON_SHAPES = {
   defaultBtn: DEFAULT_BTN_CLASSES,
   bigCircular: 'btn-floating btn-large waves-effect waves-light',
-  bigBtn: 'waves-effect waves-light btn-large btn-floating',
+  bigBtn: 'btn-large waves-effect waves-light',
 };
 
 const ACTION_BUTTONS = {
   play: {
-    color: 'amber',
+    color: '',
     icon: 'play_arrow',
   },
   pause: {
-    color: '',
+    color: 'blue lighten-2',
     icon: 'pause',
   },
   stop: {
@@ -25,12 +26,12 @@ const ACTION_BUTTONS = {
     icon: 'stop',
   },
   restart: {
-    color: '',
+    color: 'blue lighten-2',
     icon: 'loop',
   },
 
   dropdown: {
-    color: '',
+    color: 'blue lighten-2',
     icon: 'more_vert',
   },
 };
@@ -44,6 +45,7 @@ export function Button({
   actionButton,
   className,
   iconPosition,
+  href = '',
   ...rest
 }) {
   const shapeClasses = BUTTON_SHAPES[shape] || DEFAULT_BTN_CLASSES;
@@ -54,9 +56,15 @@ export function Button({
     color = color || '';
     icon = icon || '';
   }
-  return (
+
+  const button = (
     <button
-      className={classNames('btn', color, shapeClasses, className)}
+      className={classNames(
+        'btn',
+        color || DEFAULT_COLOR,
+        shapeClasses,
+        className,
+      )}
       type={type}
       {...rest}
     >
@@ -66,4 +74,25 @@ export function Button({
       {children}
     </button>
   );
+
+  const linkButon = (
+    <a
+      href={href}
+      className={classNames(
+        'btn',
+        color || DEFAULT_COLOR,
+        shapeClasses,
+        className,
+      )}
+      {...rest}
+    >
+      {icon && (
+        <i className={classNames('material-icons', iconPosition)}>{icon}</i>
+      )}
+      {children}
+    </a>
+  );
+
+  // if href prop is present, return link, otherwise button
+  return href ? linkButon : button;
 }
