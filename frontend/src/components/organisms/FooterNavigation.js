b0ae29afc1793e4a23a07b39ff2e626560a532c8
Aleksandr Gumroian <aleksandr.gumroian@gmail.com>
2019-11-25 01:41:53 +0100

    Fix footer dispaly on desktop, tablet and mobile

--- frontend/src/components/organisms/FooterNavigation.js
+++ frontend/src/components/organisms/FooterNavigation.js
@@ -9,13 +9,11 @@ export function FooterNavigationBase(props) {
     <>
       <footer className="page-footer">
         <div className="footer-copyright">
-          <div className="container">
             <div className="row">
               <div className="s12">
-                <span className={'right'}>© 2019 Pomodoro Team 2</span>
+                <span className="copyrights">© 2019 Pomodoro Team 2</span>
               </div>
             </div>
-          </div>
         </div>
       </footer>
     </>
