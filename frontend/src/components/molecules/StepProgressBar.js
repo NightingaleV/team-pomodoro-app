2af9b533b1e71c9ab847da578805e509bc22a23e
Vitezslav Slavik <vitezslavik@gmail.com>
2019-12-10 11:07:02 +0100

    usage of modulo in step progress

--- frontend/src/components/molecules/StepProgressBar.js
+++ frontend/src/components/molecules/StepProgressBar.js
@@ -3,12 +3,22 @@ import 'react-step-progress-bar/styles.css';
 import { ProgressBar, Step } from 'react-step-progress-bar';
 
 export function StepProgressBar(props) {
-  const { typeOfTimer } = props;
-
+  const { typeOfTimer, indexInCycle } = props;
+  //----------------------------------------------------------------------------
+  // Type Of Timer
+  //----------------------------------------------------------------------------
   let fill = 0;
   if (typeOfTimer === 1) fill = 0;
   if (typeOfTimer === 2) fill = 15;
   if (typeOfTimer === 3) fill = 100;
+
+  //----------------------------------------------------------------------------
+  // Timer Index
+  //----------------------------------------------------------------------------
+  let timerIndex = 0;
+  if (indexInCycle % 8 === 0) timerIndex = 8; // this is last timer in pomodoro
+  if (indexInCycle % 7 === 0) timerIndex = 7; // this is 7. timer in cycle
+  // etc
 
   return (
     <>
